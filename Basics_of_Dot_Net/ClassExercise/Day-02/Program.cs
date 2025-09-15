d504d85532505e7f264d3358e3ddec900c7c5fc2
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-09-15 15:40:03 +0530

    Class , default constructor, parameterized constructor

---
+++ Basics_of_Dot_Net/ClassExercise/Day-02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student first = new Student(); // default constructor
+
+            Student second = new Student(1,"MODI",23,"12345","modi@gmail.com"); // parameterized constructor
+            
+            first.initialize(); //without constructor explicity initialize the member of class
+            
+            first.showDisplay();
+          }
+    }
+}
---
+++ Basics_of_Dot_Net/ClassExercise/Day-02/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Student
+    {   
+       
+        int student_id;
+        string name;
+        int age;
+        string contact_no;
+        string email_id;
+
+        //public Student() { }
+        //public Student(int id,string std_name,int age,string contact_no,string email_id) {
+        //    student_id = id;
+        //    name = std_name;
+        //    this.age = age;
+        //    this.contact_no = contact_no;
+        //    this.email_id = email_id;
+        //}
+        public void initialize()
+        {
+            student_id = 10;
+            name = "Ramu Kaka";
+        }
+        public void showDisplay()
+        {
+            Console.WriteLine("Display Object");
+            Console.WriteLine(name);
+        }
+        public void getTotal()
+        {
+
+        }
+    }
+}
