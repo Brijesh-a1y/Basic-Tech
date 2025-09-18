03445ad3fc9bfcc8c17cd82f02217b350b9e8bd0
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-09-18 10:51:18 +0530

    updated dlms get set method

--- Basic_of_DLMS/Day-02/dlms.cs
+++ Basic_of_DLMS/Day-02/dlms.cs
@@ -1,3 +1,61 @@
+// void dlms_get(uint8_t obis[6]){
+//     for(int i=0; i<meterObjectCount; i++){
+//         if(obis_match(obis,meterObject[i].obis)){
+//             printf("[GET] %s = %d\n",meterObjects[i].name,meterObjects[i].value);
+//             return;
+//         }
+//     }
+//     printf("[GET] OBIS not found.\n");
+// }
+
+
+// void dlms_set(uint8_t obis[6],int32_t newValue){
+//     for(int i=0; i<meterObjectCount; i++){
+//         if(obis_match(obis,meterObject[i].obis)){
+//             printf("[SET] %s updated to  %d\n",meterObjects[i].name,newValue);
+//             return;
+//         }
+//     }
+//     printf("[SET] OBIS not found.\n");
+// }
+
+
+using System;
+using System.Collections.Generic;
+
+public class DlmsManager
+{
+    private List<MeterObject> meterObjects;
+
+    public DlmsManager()
+    {
+        // Initialize with some sample data.
+        meterObjects = new List<MeterObject>
+        {
+            new MeterObject(new byte[] { 1, 0, 1, 8, 0, 255 }, "Active energy total", 12345),
+            new MeterObject(new byte[] { 1, 0, 31, 7, 0, 255 }, "Current phase L1", 5),
+            new MeterObject(new byte[] { 1, 0, 32, 7, 0, 255 }, "Voltage phase L1", 230)
+        };
+    }
+
+    // A helper method to compare two OBIS arrays.
+    private bool ObisMatch(byte[] obis1, byte[] obis2)
+    {
+        if (obis1.Length != 6 || obis2.Length != 6)
+        {
+            return false;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            if (obis1[i] != obis2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+  
     public void DlmsGet(byte[] obis)
     {
         foreach (var meterObject in meterObjects)
@@ -25,26 +83,4 @@
         }
         Console.WriteLine("[SET] OBIS not found.");
     }
-
-
-// void dlms_get(uint8_t obis[6]){
-//     for(int i=0; i<meterObjectCount; i++){
-//         if(obis_match(obis,meterObject[i].obis)){
-//             printf("[GET] %s = %d\n",meterObjects[i].name,meterObjects[i].value);
-//             return;
-//         }
-//     }
-//     printf("[GET] OBIS not found.\n");
-// }
-
-
-// void dlms_set(uint8_t obis[6],int32_t newValue){
-//     for(int i=0; i<meterObjectCount; i++){
-//         if(obis_match(obis,meterObject[i].obis)){
-//             printf("[SET] %s updated to  %d\n",meterObjects[i].name,newValue);
-//             return;
-//         }
-//     }
-//     printf("[SET] OBIS not found.\n");
-// }
-
+}
