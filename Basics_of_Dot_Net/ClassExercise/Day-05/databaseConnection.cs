0d50cdac99f2098ed80af5fe6b850a74b37b08e8
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-09-20 15:35:33 +0530

    database connection in C#

---
+++ Basics_of_Dot_Net/ClassExercise/Day-05/databaseConnection.cs
@@ -0,0 +1,82 @@
+using System.Data.SqlClient;
+namespace TestDBMSConnectionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Getting Connection ...");
+
+            var datasource = @"LAPTOP-LF8PDD3Q"; // your server
+            var database = "dotnet_b1"; // your database name
+            //var username = "DESKTOP-LCNNVH9\\kk"; // username of server to connect
+            //var password = ""; // password
+
+            // Create your connection string
+            string connString = @"Data Source=" + datasource +
+                ";Initial Catalog=" + database + "; Trusted_Connection=True;";
+
+
+            Console.WriteLine(connString);
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                Console.WriteLine("Opening Connection ...");
+                // Open the connection
+                conn.Open();
+                Console.WriteLine("Connection successful!");
+                InsertStaff(conn);
+                displayStaff(conn);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                // Close the connection
+                conn.Close();
+            }
+
+
+
+        }
+        static void InsertStaff(SqlConnection conn)
+        {
+            Console.Write("Enter the Staff Name...");
+
+            //string myname = "esya";
+            int foodId = 2;
+            string name = "Rajma";
+            string querry = "insert into tb_food (food_id,name) values(" + foodId + ",'"+name+"')";
+            SqlCommand cm = new SqlCommand(querry, conn);
+            //cm.Parameters.AddWithValue("@name", myname);
+            int rows = cm.ExecuteNonQuery();
+            if (rows > 0)
+            {
+                Console.WriteLine("Inseted recordsuccessfully");
+            }
+
+
+        }
+        static void displayStaff(SqlConnection conn)
+        {
+            string query = "select * from tb_food";
+            SqlCommand cm = new SqlCommand(query, conn);
+            SqlDataReader reader = cm.ExecuteReader();
+            Console.WriteLine("food all data :");
+            while (reader.Read())
+            {
+                {
+                    Console.WriteLine($"food id :{reader["food_id"]}");
+                    Console.WriteLine($"Name :{reader["name"]}");
+
+                }
+            }
+        }
+    }
+}
