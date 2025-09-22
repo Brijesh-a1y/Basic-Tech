2e52e4481b87f3dc9e0bac6cc6551513ab509a75
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-09-22 15:48:40 +0530

    HTTPGET Request in C# And also varify by Postman

---
+++ Basics_of_Dot_Net/ClassExercise/Day-06/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private List<Dictionary<string, string>> student_dict = new List<Dictionary<string, string>>
+
+            {
+
+                new Dictionary<string, string>{{"id","1"},{"name","Deepika"}},
+
+                new Dictionary<string, string>{{"id","2"},{"name","Rahul"}},
+
+            };
+
+
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            return new JsonResult(student_dict);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAllStudentById(string id)
+        {
+
+            var student_details = student_dict.FirstOrDefault(p => p["id"]==id);
+            if (student_details != null) { 
+                return new JsonResult(student_details);
+            }
+                return new JsonResult("no metching record found");
+        }
+    }
+}
