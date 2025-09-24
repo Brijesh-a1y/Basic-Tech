10f8d95c497c9f10f37354bb95148147008b7ae9
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-09-24 14:20:16 +0530

    Rabbit mq

---
+++ Basics_of_RabbitMQ/Producer_Consumer/Producer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Producer.Exchanges;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "payments_user", UserName = "payments_user", Password = "1234" };
+DirectExchange directExchange = new DirectExchange(factory);
+Console.WriteLine("Enter messages to send (type 'exit' to quit):");
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input)) continue;
+    if (input.ToLower() == "exit") break;
+    await directExchange.SendMessage(input);
+
+}
